040958d6841451ca426a27700ce30e7c2f339dae
Naveed Azhar <134688534+imrofayel@users.noreply.github.com>
2024-10-12 11:06:35 +0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
     <div
       class="space-y-6 w-full p-8 flex flex-col items-center pt-12">
 
-      <a href="https://github.com/imrofayel/Gitfyel" target="_blank"><div class="bg-white rounded-full text-lg px-3 py-1 border shadow-inner flex items-center">A project by Naveed Azhar&ThinSpace;
+      <a href="https://github.com/imrofayel/Gitfyel" target="_blank"><div class="bg-white rounded-full text-lg px-3 py-1 border shadow-inner flex items-center">A project by Adam Rofayel&ThinSpace;
 
         <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 24 24" class="inline"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h10v10M7 17L17 7"/></svg>
 
